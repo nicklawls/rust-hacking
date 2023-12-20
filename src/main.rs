73268f029a375fe81399642831c4966f46bbad39
Nick Lawler <nick@nicklawler.website>
2023-12-20 16:21:11 -0500

    wherever I was

--- src/main.rs
+++ src/main.rs
@@ -1,9 +1,37 @@
+fn incr(n: &mut i32) {
+    *n += 1;
+}
 fn main() {
-    let mut vec: Vec<i32> = vec![1, 2, 3];
-    let num = &mut vec[2];
+    let mut n = 1;
+    incr(&mut n);
+    println!("{n}");
+}
 
-    *num += 1;
-    println!("Third element is now {}", *num);
+fn first(strings: &Vec<String>) -> &String {
+    let s_ref = &strings[0];
+    return s_ref;
+}
 
-    println!("Vector is now {:?}", vec);
+// fn first_or(strings: &Vec<String>, default: &String) -> &String {
+//     if strings.len() > 0 {
+//         &strings[0]
+//     } else {
+//         default
+//     }
+// }
+
+// fn return_a_string() -> &String {
+//     let s = String::from("Hello");
+//     let s_ref = &s;
+//     s_ref;
+// }
+
+fn printo() -> () {
+    let s = String::from("hi");
+    println!("{s}")
+}
+
+fn give_and_take(v: &Vec<i32>, n: i32) -> i32 {
+    v.push(n);
+    v.remove(0)
 }
