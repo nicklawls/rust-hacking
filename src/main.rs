73fb454f17fccf0473aefe3d0d23964b811c2fcb
Nick Lawler <nick@nicklawler.website>
2023-12-20 22:04:43 -0600

    Trying the query problem
    Tried to design it "flatter" to avoid problems with recursive types
    indexing with `usize` because there's some crazy ass trait for `get`
    Still not sure why I need to deref op_index

--- src/main.rs
+++ src/main.rs
@@ -1,37 +1,63 @@
-fn incr(n: &mut i32) {
-    *n += 1;
+#[derive(Debug, PartialEq, Eq)]
+
+enum Op {
+    And,
+    Or,
 }
+
+#[derive(Debug)]
+enum QueryPart {
+    Equals { field: String, operand: String },
+    SubQuery { operator: Op, operand: Vec<usize> },
+}
+
+#[derive(Debug)]
+struct FullQuery {
+    storage: Vec<QueryPart>,
+    root: usize,
+}
+
+impl FullQuery {
+    fn to_sql(self: &FullQuery) -> Option<String> {
+        if let Some(root_node) = self.storage.get(self.root) {
+            return Some(self.to_sub_sql(root_node));
+        } else {
+            return None;
+        }
+    }
+
+    fn to_sub_sql(self: &FullQuery, part: &QueryPart) -> String {
+        match part {
+            QueryPart::Equals { field, operand } => format!("{}={}", field, operand),
+            QueryPart::SubQuery { operator, operand } => operand
+                .iter()
+                .filter_map(|op_index| self.storage.get(*op_index))
+                .map(|subquery| format!("({})", &self.to_sub_sql(subquery) ))
+                .collect::<Vec<String>>()
+                .join(if *operator == Op::And { " AND " } else { " OR " }),
+        }
+    }
+}
+
 fn main() {
-    let mut n = 1;
-    incr(&mut n);
-    println!("{n}");
+    let example = FullQuery {
+        root: 2,
+        storage: Vec::from([
+            QueryPart::Equals {
+                field: "a".to_string(),
+                operand: "2".to_string(),
+            },
+            QueryPart::Equals {
+                field: "a".to_string(),
+                operand: "3".to_string(),
+            },
+            QueryPart::SubQuery {
+                operator: Op::Or,
+                operand: Vec::from([0, 1]),
+            },
+        ]),
+    };
+
+    println!("{:?}", example.to_sql())
 }
 
-fn first(strings: &Vec<String>) -> &String {
-    let s_ref = &strings[0];
-    return s_ref;
-}
-
-// fn first_or(strings: &Vec<String>, default: &String) -> &String {
-//     if strings.len() > 0 {
-//         &strings[0]
-//     } else {
-//         default
-//     }
-// }
-
-// fn return_a_string() -> &String {
-//     let s = String::from("Hello");
-//     let s_ref = &s;
-//     s_ref;
-// }
-
-fn printo() -> () {
-    let s = String::from("hi");
-    println!("{s}")
-}
-
-fn give_and_take(v: &Vec<i32>, n: i32) -> i32 {
-    v.push(n);
-    v.remove(0)
-}
