a5db5bc605a2f527d548fa54e2ee4463b47382cf
Nick Lawler <nick@nicklawler.website>
2024-01-01 23:39:15 -0600

    intermiadite

--- src/bin/decode.rs
+++ src/bin/decode.rs
@@ -12,14 +12,14 @@ fn main() -> io::Result<()> {
         let fl = fs::File::open(filename)?;
         let file_len = fl.metadata()?.len();
         eprintln!("{}, {:#?} B", filename, file_len);
-        let instruction_stream = io::Read::bytes(io::BufReader::new(fl));
-        
-
-        let y = instruction_stream.map(|x| x.expect("oops"));
+        let instruction_stream = io::Read::bytes(io::BufReader::new(fl))
+            // have to map away results. A bit hacky, but avoids collecting
+            // too eagerly.
+            .map(|x| x.expect("oops"));
 
         // read file lines, get bytes from str, then group into u16s
-        let decoded_instruction_stream =
-            decode_instruction_stream(y).map_err(|e| io::Error::new(io::ErrorKind::Other, e))?;
+        let decoded_instruction_stream = decode_instruction_stream(instruction_stream)
+            .map_err(|e| io::Error::new(io::ErrorKind::Other, e))?;
 
         println!("{decoded_instruction_stream:#?}");
     }
