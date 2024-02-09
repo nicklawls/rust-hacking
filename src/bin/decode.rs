99c196e653654acfcf5d679124b5773248dfd232
Nick Lawler <nick@nicklawler.website>
2024-02-08 23:39:20 -0500

    Script test for comparing with NASM
    Simplify the rust binary to just take a filename argument.
    
    Could be more like the previous rust binary design, looping over an
    array of the files.

--- src/bin/decode.rs
+++ src/bin/decode.rs
@@ -1,40 +1,31 @@
 use rustbook::decoder;
-use std::{convert::identity, fs, io};
-
-const FILES: [&str; 1] = [
-    // "listing_0037_single_register_mov",
-    // "listing_0038_many_register_mov",
-    // "listing_0039_more_movs",
-    // "listing_0040_challenge_movs",
-    "listing_0041_add_sub_cmp_jnz",
-];
+use std::{convert::identity, env, fs, io};
 
 fn main() -> io::Result<()> {
-    for filename in FILES {
-        let fl = fs::File::open(filename)?;
-        let file_len = fl.metadata()?.len();
-        eprintln!("{}, {:#?} B", filename, file_len);
-        let instruction_stream = io::Read::bytes(io::BufReader::new(fl))
-            // have to map away results. A bit hacky, but avoids collecting
-            // too eagerly.
-            .map(|x| x.expect("oops"));
-        println!("bits 16");
-        println!("");
+    let filename = env::args().nth(1).expect("file not found");
+    let fl = fs::File::open(filename.to_string())?;
+    let file_len = fl.metadata()?.len();
+    eprintln!("{}, {:#?} B", filename, file_len);
+    let instruction_stream = io::Read::bytes(io::BufReader::new(fl))
+        // have to map away results. A bit hacky, but avoids collecting
+        // too eagerly.
+        .map(|x| x.expect("oops"));
+    println!("bits 16");
+    println!("");
 
-        for instruction in decoder::decode_instruction_stream(instruction_stream)
-            .map_or_else(
-                |(instructions, error)| {
-                    eprintln!("{error:#?}");
-                    instructions
-                },
-                identity,
-            )
-            .iter()
-            .map(decoder::pp_asm)
-            .collect::<Vec<String>>()
-        {
-            println!("{instruction}");
-        }
+    for instruction in decoder::decode_instruction_stream(instruction_stream)
+        .map_or_else(
+            |(instructions, error)| {
+                eprintln!("{error:#?}");
+                instructions
+            },
+            identity,
+        )
+        .iter()
+        .map(decoder::pp_asm)
+        .collect::<Vec<String>>()
+    {
+        println!("{instruction}");
     }
     Ok(())
 }
--- test.ts
+++ test.ts
@@ -1,11 +1,43 @@
-import { $ } from 'bun';
-
-const [file] = Bun.argv;
+import { $ } from "bun";
 
 // take in the filename of one of the hosted asm files
-// if not found, download it
+const [, , asmFilename] = Bun.argv;
+
+if (!asmFilename) {
+  throw "No filename arg";
+}
+if (asmFilename.includes(".out.") || !asmFilename.endsWith(".asm")) {
+  throw "Not a source ASM file ".concat(asmFilename);
+}
+
+const asmFile = Bun.file(asmFilename);
+
+if (!asmFile.exists()) {
+  throw "File does not exist";
+  // TODO: if not found, download it
+}
+
 // assemble it, generating the suffix-less version
-// pipe the suffix-less version into `cargo run --bin decode`, pipe the output to 
+await $`nasm ${asmFile}`;
+
+// pass the suffix-less version into `cargo run --bin decode`, pipe the output to
 // a .out.asm file
+const binaryFileName = asmFilename.split(".asm")[0];
+if (!binaryFileName) {
+  throw "bad binary filename ".concat(String(binaryFileName));
+}
+
+const outAsmFileName = `${binaryFileName}.out.asm`;
+
+await $`cargo run --bin decode -- ${binaryFileName} > ${outAsmFileName}`;
+
 // generate a .out file by running nasm on the .out.asm
-// diff the .out and the suffix-less file 
+await $`nasm ${outAsmFileName}`;
+
+// diff the .out and the suffix-less file
+const outFileName = `${binaryFileName}.out`;
+const { exitCode } = await $`diff ${binaryFileName} ${outFileName}`;
+
+if (exitCode === 0) {
+  console.log("Success!");
+}
