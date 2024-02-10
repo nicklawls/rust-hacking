8458f5bcd97f23be211a65c1ad8ae21758519b18
Nick Lawler <nick@nicklawler.website>
2024-02-09 23:40:41 -0500

    Parallelize tests across all files
    Man that was easy.
    
    Also delete an old file

--- test.ts
+++ test.ts
@@ -1,46 +1,53 @@
 import { $ } from "bun";
 
 // take in the filename of one of the hosted asm files
-const [, , asmFilename] = Bun.argv;
+const files = [
+  "listing_0037_single_register_mov.asm",
+  "listing_0038_many_register_mov.asm",
+  "listing_0039_more_movs.asm",
+  "listing_0040_challenge_movs.asm",
+  "listing_0041_add_sub_cmp_jnz.asm",
+];
 
-if (!asmFilename) {
-  throw "No filename arg";
-}
-if (asmFilename.includes(".out.") || !asmFilename.endsWith(".asm")) {
-  throw "Not a source ASM file ".concat(asmFilename);
-}
+const testFile = async (asmFilename: string) => {
+  if (asmFilename.includes(".out.") || !asmFilename.endsWith(".asm")) {
+    throw "Not a source ASM file ".concat(asmFilename);
+  }
 
-const asmFile = Bun.file(asmFilename);
+  const asmFile = Bun.file(asmFilename);
 
-if (!asmFile.exists()) {
-  throw "File does not exist";
-  // TODO: if not found, download it
-}
+  if (!asmFile.exists()) {
+    throw "File does not exist";
+    // TODO: if not found, download it
+  }
 
-// assemble it, generating the suffix-less version
-await $`nasm ${asmFile}`;
+  // assemble it, generating the suffix-less version
+  await $`nasm ${asmFile}`;
 
-// pass the suffix-less version into `cargo run --bin decode`, pipe the output to
-// a .out.asm file
-const binaryFileName = asmFilename.split(".asm")[0];
-if (!binaryFileName) {
-  throw "bad binary filename ".concat(String(binaryFileName));
-}
+  // pass the suffix-less version into `cargo run --bin decode`, pipe the output to
+  // a .out.asm file
+  const binaryFileName = asmFilename.split(".asm")[0];
+  if (!binaryFileName) {
+    throw "bad binary filename ".concat(String(binaryFileName));
+  }
 
-const outAsmFileName = `${binaryFileName}.out.asm`;
+  const outAsmFileName = `${binaryFileName}.out.asm`;
 
-await $`cargo run --bin decode -- ${binaryFileName} > ${outAsmFileName}`;
+  await $`cargo run --bin decode -- ${binaryFileName} > ${outAsmFileName}`;
 
-// generate a .out file by running nasm on the .out.asm
-const reassembly = await $`nasm ${outAsmFileName}`;
-if (reassembly.exitCode) {
-  throw "failed to reassemble"
-}
+  // generate a .out file by running nasm on the .out.asm
+  const reassembly = await $`nasm ${outAsmFileName}`;
+  if (reassembly.exitCode) {
+    throw "failed to reassemble";
+  }
 
-// diff the .out and the suffix-less file
-const outFileName = `${binaryFileName}.out`;
-const { exitCode } = await $`diff ${binaryFileName} ${outFileName}`;
+  // diff the .out and the suffix-less file
+  const outFileName = `${binaryFileName}.out`;
+  const { exitCode } = await $`diff ${binaryFileName} ${outFileName}`;
 
-if (exitCode === 0) {
-  console.log("Success!");
-}
+  if (exitCode === 0) {
+    console.log({ [asmFilename]: "Success!" });
+  }
+};
+
+await Promise.all(files.map(testFile));
