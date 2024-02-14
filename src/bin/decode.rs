7a92190f20b6eb30e40c06021d38df1ccab03bc1
Nick Lawler <nick@nicklawler.website>
2024-02-13 20:23:08 -0500

    Handle file read errors without expect
    Exhaust the iterator on first error. Similar to the instruction stream
    itself, but instead of preserving result we yank it out and leave an
    error message. User should be able to see both that error and whatever
    "missing byte" error from the decoder

--- src/bin/decode.rs
+++ src/bin/decode.rs
@@ -8,12 +8,16 @@ fn main() -> io::Result<()> {
     let fl = fs::File::open(filename.to_string())?;
     let file_len = fl.metadata()?.len();
     eprintln!("{}, {:#?} B", filename, file_len);
-    let instruction_stream = io::Read::bytes(io::BufReader::new(fl))
-        // have to map away results. A bit hacky, but avoids collecting
-        // too eagerly.
-        .map(|x| x.expect("oops"));
 
-    let instructions = decoder::decode_instruction_stream(instruction_stream);
+    let instructions = decoder::decode_instruction_stream(
+        io::Read::bytes(io::BufReader::new(fl)).map_while(|byte_res| match byte_res {
+            Ok(byte) => Some(byte),
+            Err(err) => {
+                eprintln!("Error reading file bytes: {err:#?}");
+                None
+            }
+        }),
+    );
 
     println!("bits 16");
     println!("");
