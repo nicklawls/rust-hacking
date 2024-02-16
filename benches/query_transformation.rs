9e23e3e185578c70dee852b697a24386bb12e453
Nick Lawler <nick@nicklawler.website>
2024-02-16 14:20:47 -0500

    Fix import

--- benches/query_transformation.rs
+++ benches/query_transformation.rs
@@ -1,5 +1,5 @@
 use criterion::{black_box, criterion_group, criterion_main, Criterion};
-use rustbook::QueryBoxed;
+use rustbook::query_transformation::QueryBoxed;
 
 pub fn criterion_benchmark(c: &mut Criterion) {
     let data = r#"
